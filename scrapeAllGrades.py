c1c2225c3e98de1e89e177af78ee27687637cd51
Owen Busler <owen1050@gmail.com>
2024-12-06 23:12:11 -0500

    yudpat

--- scrapeAllGrades.py
+++ scrapeAllGrades.py
@@ -3,12 +3,10 @@ import time
 
 class GradeScraper:
 
-	def __init__(self, loginURL, username, password):
-		self.url = loginURL
-		self.un = username
-		self.pw = password
+	def __init__(self, url):
+		self.url = url
 
-	def getAllGrades(self, classList, headless = True, printUpdates = True):
+	def getUrl(self, headless = True, printUpdates = True):
 
 		options = webdriver.ChromeOptions()
 		if(headless):
@@ -25,91 +23,7 @@ class GradeScraper:
 		driver.get(self.url)
 
 
-		#type username
-		nameXpath = '/html/body/section[1]/article[2]/div/div/form/div[1]/div/div/div[1]/input'
-		unameBox = driver.find_element_by_xpath(nameXpath)
 
-		unameBox.send_keys(self.un)
-
-		#type password
-		pwXpath = '/html/body/section[1]/article[2]/div/div/form/div[1]/div/div/div[2]/input'
-		pwBox = driver.find_element_by_xpath(pwXpath)
-
-		pwBox.send_keys(self.pw)
-
-
-		signInXpath = "/html/body/section[1]/article[2]/div/div/form/div[1]/div/div/div[4]/button"
-		signInButton = driver.find_element_by_xpath(signInXpath)
-		  
-		signInButton.click()
-
-		driver.set_window_size(1800, 800)
-
-		#wait for load
-		time.sleep(2)
-
-		firstStudentNameXpath = 	'/html/body/div[4]/div[5]/div[4]/div[2]/div/div[2]/table/tbody/tr[{index}]/td[2]/a[1]'
-		firstStudentPerXpath = 		'/html/body/div[4]/div[5]/div[4]/div[2]/div/div[2]/table/tbody/tr[{index}]/td[3]'
-		firstStudentLetterXpath = 	'/html/body/div[4]/div[5]/div[4]/div[2]/div/div[2]/table/tbody/tr[{index}]/td[4]/div/span'
-																									#STUDNET INDEX
-		secondStudentNamXpath = 	'/html/body/div[4]/div[5]/div[4]/div[2]/div/div[2]/table/tbody/tr[2]/td[2]/a[1]'
-
-		gradebooksOptionsXpath = '/html/body/div[4]/nav[1]/div/ul/li[2]/a'
-
-		onlyOnceGradebookIsSelectedFirstClassXpath = 	'/html/body/div[4]/nav[1]/div/ul/li[2]/div/ul/li[{index}]/a'
-		onlyOnceGradebookIsSelectedSecondClassXpath =	'/html/body/div[4]/nav[1]/div/ul/li[2]/div/ul/li[2]/a'
-
-		nameOfClassXPATH =							 	'/html/body/div[4]/nav[1]/div/ul/li[2]/div/ul/li[{index}]/a/span[1]'
-		classDescXPATH = 								'/html/body/div[4]/nav[1]/div/ul/li[2]/div/ul/li[{index}]/a/span[2]'
-
-		classs = classList
-
-		allGrades = {}
-
-		for c in classs:
-			grades = []
-			gradebookButton = driver.find_element_by_xpath(gradebooksOptionsXpath)
-			gradebookButton.click()
-
-			time.sleep(2)
-
-			firstClassButton = driver.find_element_by_xpath(onlyOnceGradebookIsSelectedFirstClassXpath.format(index=c))
-
-			classNameHTML = driver.find_element_by_xpath(nameOfClassXPATH.format(index=c))
-			className = classNameHTML.get_attribute('innerHTML')
-
-			classDescHTML = driver.find_element_by_xpath(classDescXPATH.format(index=c))
-			classDesc = classDescHTML.get_attribute('innerHTML')
-			
-			classNamePlusDesc = className + ":" + classDesc
-			if(printUpdates):
-				print("Getting grades for: " + classNamePlusDesc)
-
-
-			firstClassButton.click()
-			
-			time.sleep(5)
-			student = 1
-			while(True):
-				try:
-					firstStudnetHTML = driver.find_element_by_xpath(firstStudentNameXpath.format(index=student))
-					name = firstStudnetHTML.get_attribute('innerHTML')
-
-					firstPerHTML = driver.find_element_by_xpath(firstStudentPerXpath.format(index=student))
-					per = firstPerHTML.get_attribute('innerHTML')
-
-					firstLetterHTML = driver.find_element_by_xpath(firstStudentLetterXpath.format(index=student))
-					letter = firstLetterHTML.get_attribute('innerHTML')
-					student = student + 1
-
-					grades.append([name, per,  letter])
-
-				except:
-					break
-
-			allGrades[classNamePlusDesc] = grades
-
-			time.sleep(1)
 
 		driver.quit()
-		return allGrades
+		return 0
--- test.py
+++ test.py
@@ -1,15 +1,10 @@
 # import web driver
 from scrapeAllGrades import GradeScraper
 
-username = "buslero@villawalsh.org"
-#read password from file so its not on github
-f = open("password", "r")
-password = f.read()
-f.close()
 
-url = "https://gb5.plusportals.com/login/loginView?qstr=JQwDzcIrLUDwAbJIIqftfpnaY2o/4a2YbuA6r0VqlTIEtEQ6R19uOLvbazYgIpUH"
+url = "https://www.wunderground.com/maps/satellite/regional-infrared/usaec"
 
-gs = GradeScraper(url, username, password)
+gs = GradeScraper(url)
 
-grades = gs.getAllGrades([1,2,3,4], headless=False, printUpdates=False)
+grades = gs.getUrl()
 print(grades)
