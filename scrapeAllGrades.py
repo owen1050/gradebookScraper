85d907cd205871936c23f4a861a71095ac18412e
Owen Busler <owen1050@gmail.com>
2024-12-06 23:21:07 -0500

    Update scrapeAllGrades.py

--- scrapeAllGrades.py
+++ scrapeAllGrades.py
@@ -1,13 +1,18 @@
 from selenium import webdriver
 import time
 
+url = "https://www.wunderground.com/maps/satellite/regional-infrared/usaec"
+xpath = "//*[@id=\"main-page-content\"]/section/div[1]/div[2]/div[2]/img[1]"
+
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
 driver = webdriver.Chrome('chromedriver.exe', options=options)
 
 # driver.get method() will navigate to a page given by the URL address
-driver.get(self.url)
+driver.get(url)
 time.sleep(3)
 
-gifElement = driver.find_element_by_xpath("//*[@id=\"main-page-content\"]/section/div[1]/div[2]/div[2]/img[1]")
+gifElement = driver.find_element_by_xpath(xpath)
 urlName = gifElement.get_attribute('src')
 driver.quit()
 print(urlName)
